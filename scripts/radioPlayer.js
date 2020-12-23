3c47cdcb757fd4f4b08d2fd768a527cb74c664ca
dimasss91 <65185213+dimasss91@users.noreply.github.com>
2020-12-23 13:20:55 +0300

    Add files via upload
    radio work

--- scripts/radioPlayer.js
+++ scripts/radioPlayer.js
@@ -1,2 +1,72 @@
-export const radioPlayerInit = () => {console.log('Radio Player');
+export const radioPlayerInit = () => {
+   console.log('Radio Player');
+
+   const radio = document.querySelector('.radio');
+   const radioNavigation = document.querySelector('.radio-navigation');
+   const radioCoverImg = document.querySelector('.radio-cover__img');
+   const radioItem = document.querySelectorAll('.radio-item');
+   const radioHeaderBig = document.querySelector('.radio-header__big');
+   const radioStop = document.querySelector('.radio-stop');
+
+   const audio = new Audio();
+   audio.type = 'audio/aac';
+
+   radioStop.disabled = true;
+
+   const changeIconPlay = () => {
+      if (audio.paused) {
+         radio.classList.remove('play');
+         radioStop.classList.add('fa-play');
+         radioStop.classList.remove('fa-stop');
+         
+      } else {
+         radio.classList.add('play');
+         radioStop.classList.add('fa-stop');
+         radioStop.classList.remove('fa-play');
+      }
+   };
+
+   const selectItem = elem => {
+      radioItem.forEach(item => item.classList.remove('select'));
+      elem.classList.add('select');
+   }
+
+   radioNavigation.addEventListener('change', event => {
+      const target = event.target;
+      const parrent = target.closest('.radio-item');
+      // выбор елемент (движ вверх)
+      selectItem(parrent);
+
+      const title = parrent.querySelector('.radio-name').textContent;
+      // получаем имя станции из тега с классом .radio-name.
+      // .textConten достает текст \содержимое 
+      radioHeaderBig.textContent = title;
+
+      const urlImg = parrent.querySelector('.radio-img').src;
+      radioCoverImg.src = urlImg
+      // сменить изображенеи 
+
+      radioStop.disabled = false;
+      audio.src = target.dataset.radioStantion;
+
+      audio.play();
+      changeIconPlay();
+
+      // data-radio-stantion
+
+   });
+   // переключать мужду радио (1 раз на станцию, пока не переключишь)
+
+   radioStop.addEventListener('click', () => {
+      if (audio.pauser) {
+         audio.play();
+      } else {
+         audio.pause();
+      }
+      changeIconPlay();
+   });
 };
+
+
+// pause    метод
+// paused   свойство
